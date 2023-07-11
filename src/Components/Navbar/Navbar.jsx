fa25e78e80bd40e03649946d33eb297d6783d98f
Dwain <dwaincontact@gmail.com>
2023-07-11 11:03:25 +0200

    ajout du responsive de la navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Navbar.css"
 import logo from "../../Assets/logo.png";
 import iconSearch from "../../Assets/icon-search.png"
@@ -7,10 +7,16 @@ import iconAccount from "../../Assets/icon-account.png"
 import iconBasket from "../../Assets/icon-basket.png"
 
 function Navbar(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
     return(
         <div className="container-nav">
-            <nav>
-                <ul className="nav-links">
+            <nav className={isMenuOpen ? "nav-open" : ""}>
+                <ul className={`nav-links ${isMenuOpen ? "" : "list-none"}`}>
                     <li className="links">accueil</li>
                     <li className="links">shop</li>
                     <li className="links">inspirations</li>
@@ -20,14 +26,14 @@ function Navbar(){
                     <img src={logo} alt="" className="logo" />
                 </div>
 
-                <ul className="nav-action-account">
+                <ul className={`nav-action-account ${isMenuOpen ? "" : "list-none"}`}>
                     <li className="action-account"><img src={iconSearch} alt="" /></li>
                     <li className="action-account"><img src={iconAccount} alt="" /></li>
                     <li className="action-account"><img src={iconHeart} alt="" /></li>
                     <li className="action-account"><img src={iconBasket} alt="" /></li>
-                </ul>
+                </ul> 
 
-                <div className="container-icon-menu">
+                <div className="container-icon-menu" onClick={handleMenuClick}>
                     <ion-icon className="icon-menu" name="menu-outline"></ion-icon>
                 </div>
             </nav>
